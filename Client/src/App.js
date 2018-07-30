2a4019a515ef846b48c82b34485a3a7f1d4f95a6
Ratnesh <ratnesh.chauhan@Arthrex.com>
2018-07-30 07:12:29 -0700

    Updated react error boundary package react-error-boundary-component-fallback2 from NPM which supports onclick even listening

--- Client/src/App.js
+++ Client/src/App.js
@@ -1,13 +1,20 @@
 import React, { Component } from 'react';
 import ChatBox from './ChatBoxComponent'
 // Re-using my ErrorBoundary Component 
-import ErrorBoundary from 'react-error-boundary-component-fallback';
+import ErrorBoundary from 'react-error-boundary-component-fallback2';
 
 
 class App extends Component {
+  refreshPage(){
+    window.location.reload();
+  }
+
   render() {
     return (
-      <ErrorBoundary headerColor="lightseagreen" errorTitle="Server Error" errorText="Unable to not connect you to the Chat Room Server. Please refresh this page and try again!" buttonType={['', 'primary', '', '']} buttonLabel={['', 'OK', '', '']} modal={true} autoScrollBodyContent={false} customContentStyle={null}>
+      <ErrorBoundary headerColor="lightseagreen" errorTitle="Server Error" 
+      errorText="Unable to not connect you to the Chat Room Server. Please refresh this page and try again!" 
+      buttonType={['', 'primary', '', '']} buttonLabel={['', 'Refresh', '', '']} modal={true} autoScrollBodyContent={false}
+      customContentStyle={null} onClick={this.refreshPage}>
         <ChatBox />
       </ErrorBoundary>
 
